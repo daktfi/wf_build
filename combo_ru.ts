e40ffe1c8a5a83e0252ea456681845622109868c
Dmitry Kuteynikov <daktfi@mail.ru>
2018-10-22 18:39:27 +0300

    Create combo_ru.ts

---
+++ combo_ru.ts
@@ -0,0 +1,492 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <source>Warframe weapon build calculator</source>
+        <translation>Калькулятор сборок оружия для Warframe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>Calculate</source>
+        <translation>Рассчёт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>Sustained DPS</source>
+        <translation>Урон/перезарядка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <location filename="mainwindow.ui" line="246"/>
+        <location filename="mainwindow.ui" line="408"/>
+        <location filename="mainwindow.ui" line="1073"/>
+        <location filename="mainwindow.ui" line="1171"/>
+        <location filename="mainwindow.ui" line="1239"/>
+        <location filename="mainwindow.ui" line="1307"/>
+        <location filename="mainwindow.ui" line="1418"/>
+        <location filename="mainwindow.ui" line="1477"/>
+        <location filename="mainwindow.ui" line="1512"/>
+        <location filename="mainwindow.ui" line="1531"/>
+        <location filename="mainwindow.ui" line="1560"/>
+        <location filename="mainwindow.ui" line="1579"/>
+        <location filename="mainwindow.ui" line="1618"/>
+        <location filename="mainwindow.ui" line="1637"/>
+        <location filename="mainwindow.ui" line="1793"/>
+        <location filename="mainwindow.ui" line="1822"/>
+        <location filename="mainwindow.ui" line="1851"/>
+        <location filename="mainwindow.ui" line="1880"/>
+        <location filename="mainwindow.ui" line="1909"/>
+        <source>0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <location filename="mainwindow.ui" line="253"/>
+        <location filename="mainwindow.ui" line="415"/>
+        <source>Phys</source>
+        <translation>Физический</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <location filename="mainwindow.ui" line="96"/>
+        <location filename="mainwindow.ui" line="260"/>
+        <location filename="mainwindow.ui" line="274"/>
+        <location filename="mainwindow.ui" line="422"/>
+        <location filename="mainwindow.ui" line="436"/>
+        <source>99</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <location filename="mainwindow.ui" line="267"/>
+        <location filename="mainwindow.ui" line="429"/>
+        <source>Element</source>
+        <translation>Элементальный</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <location filename="mainwindow.ui" line="505"/>
+        <source>1.00</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <location filename="mainwindow.ui" line="526"/>
+        <location filename="mainwindow.ui" line="1445"/>
+        <source>Puncture</source>
+        <translation>Колющий</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="139"/>
+        <location filename="mainwindow.ui" line="512"/>
+        <location filename="mainwindow.ui" line="1435"/>
+        <source>Impact</source>
+        <translation>Ударный</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="146"/>
+        <location filename="mainwindow.ui" line="153"/>
+        <location filename="mainwindow.ui" line="160"/>
+        <location filename="mainwindow.ui" line="223"/>
+        <source>1000</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="167"/>
+        <location filename="mainwindow.ui" line="540"/>
+        <location filename="mainwindow.ui" line="1455"/>
+        <source>Slash</source>
+        <translation>Режущий</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="174"/>
+        <location filename="mainwindow.ui" line="645"/>
+        <source>20.67</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <location filename="mainwindow.ui" line="638"/>
+        <location filename="mainwindow.ui" line="1644"/>
+        <source>RoF</source>
+        <translation>Скорострельность</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="188"/>
+        <source>200</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="195"/>
+        <location filename="mainwindow.ui" line="666"/>
+        <location filename="mainwindow.ui" line="1858"/>
+        <source>Reload</source>
+        <translation>Перезарядка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="202"/>
+        <location filename="mainwindow.ui" line="652"/>
+        <location filename="mainwindow.ui" line="1829"/>
+        <source>Magazine</source>
+        <translation>Магазин</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="209"/>
+        <source>0.11</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="216"/>
+        <location filename="mainwindow.ui" line="610"/>
+        <location filename="mainwindow.ui" line="1726"/>
+        <source>Pellets</source>
+        <translatorcomment>Или дробины</translatorcomment>
+        <translation>Пули</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="239"/>
+        <source>Single shot</source>
+        <translation>Выстрел</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="297"/>
+        <location filename="mainwindow.ui" line="371"/>
+        <location filename="mainwindow.ui" line="617"/>
+        <location filename="mainwindow.ui" line="631"/>
+        <location filename="mainwindow.ui" line="732"/>
+        <location filename="mainwindow.ui" line="1719"/>
+        <location filename="mainwindow.ui" line="1748"/>
+        <source>1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="302"/>
+        <location filename="mainwindow.ui" line="737"/>
+        <source>2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="307"/>
+        <location filename="mainwindow.ui" line="742"/>
+        <source>3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="312"/>
+        <location filename="mainwindow.ui" line="747"/>
+        <source>4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="317"/>
+        <location filename="mainwindow.ui" line="752"/>
+        <source>5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="322"/>
+        <location filename="mainwindow.ui" line="757"/>
+        <source>6</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="327"/>
+        <location filename="mainwindow.ui" line="762"/>
+        <source>7</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="332"/>
+        <location filename="mainwindow.ui" line="767"/>
+        <source>8</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="343"/>
+        <location filename="mainwindow.ui" line="582"/>
+        <location filename="mainwindow.ui" line="1596"/>
+        <source>Crit X</source>
+        <translation>х Крит</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="350"/>
+        <location filename="mainwindow.ui" line="596"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="357"/>
+        <source>Elemental</source>
+        <translation>Элементальный</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="364"/>
+        <source>1.25</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="378"/>
+        <source>11,7</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="385"/>
+        <location filename="mainwindow.ui" line="568"/>
+        <location filename="mainwindow.ui" line="1538"/>
+        <source>Crit %</source>
+        <translation>Шанс крита</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="401"/>
+        <source>Burst DPS</source>
+        <translation>Урон/сек</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="477"/>
+        <location filename="mainwindow.ui" line="1360"/>
+        <source>Description</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="484"/>
+        <source>Rifle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="491"/>
+        <location filename="mainwindow.ui" line="1335"/>
+        <source>Normal</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="498"/>
+        <location filename="mainwindow.ui" line="1771"/>
+        <source>Disposition</source>
+        <translation>Диспозиция</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="519"/>
+        <location filename="mainwindow.ui" line="533"/>
+        <location filename="mainwindow.ui" line="547"/>
+        <location filename="mainwindow.ui" line="561"/>
+        <source>100.1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="554"/>
+        <source>none</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="575"/>
+        <location filename="mainwindow.ui" line="603"/>
+        <source>0.33</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="589"/>
+        <source>2.0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="624"/>
+        <source>rnds/shot</source>
+        <translation>Патронов/выстрел</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="659"/>
+        <source>100</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="673"/>
+        <source>0.95</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="680"/>
+        <location filename="mainwindow.ui" line="1800"/>
+        <source>Ammo</source>
+        <translation>Боекомплект</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="687"/>
+        <source>1600</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="814"/>
+        <source>Mods</source>
+        <translation>Модов</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="847"/>
+        <source>Recursion</source>
+        <translation>Рекурсия</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="854"/>
+        <source>Find riven</source>
+        <translation>Подобратьи МР</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="861"/>
+        <source>Find build</source>
+        <translation>Найти сборку</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="895"/>
+        <source>+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="920"/>
+        <source>-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="933"/>
+        <location filename="mainwindow.ui" line="1031"/>
+        <location filename="mainwindow.ui" line="1396"/>
+        <source>Name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="940"/>
+        <location filename="mainwindow.ui" line="947"/>
+        <location filename="mainwindow.ui" line="954"/>
+        <location filename="mainwindow.ui" line="961"/>
+        <source>999</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="968"/>
+        <location filename="mainwindow.ui" line="1093"/>
+        <source>Stat1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="975"/>
+        <location filename="mainwindow.ui" line="1194"/>
+        <source>Stat3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="982"/>
+        <location filename="mainwindow.ui" line="1149"/>
+        <source>Stat2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="989"/>
+        <location filename="mainwindow.ui" line="1249"/>
+        <source>Stat4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1002"/>
+        <source>Description
+Description
+Description
+</source>
+        <translation>Description
+Description
+Description</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1025"/>
+        <source>Edit Mods</source>
+        <translation>Редактировать моды</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1041"/>
+        <location filename="mainwindow.ui" line="1103"/>
+        <location filename="mainwindow.ui" line="1201"/>
+        <location filename="mainwindow.ui" line="1272"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1051"/>
+        <location filename="mainwindow.ui" line="1425"/>
+        <source>Type</source>
+        <translation>Тип оружия</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1113"/>
+        <source>Weapon</source>
+        <translation>Одно оружие</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1324"/>
+        <source>SubType</source>
+        <translation>Подтип</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1343"/>
+        <location filename="mainwindow.ui" line="1680"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1350"/>
+        <location filename="mainwindow.ui" line="1673"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1371"/>
+        <source>Edit Weapons</source>
+        <translation>Редактировать оружие</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1586"/>
+        <source>Status %</source>
+        <translation>Шанс статуса</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1687"/>
+        <source>Desc</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1697"/>
+        <source>Rnd/Shot</source>
+        <translation>Патронов/выстрел</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1761"/>
+        <source>Subtype</source>
+        <translation>Подтип</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1887"/>
+        <source>each rnd</source>
+        <translation>за патрон</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1980"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="22"/>
+        <source>Cannot open database</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="23"/>
+        <source>Unable to establish a database connection.
+This program needs SQLite support.
+
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
